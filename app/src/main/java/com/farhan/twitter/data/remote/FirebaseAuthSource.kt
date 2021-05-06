707546a6b7ceebea21a36ef77a31beaba395d586
Farhan <farhan@notesgen.com>
2021-05-06 21:44:26 +0400

    Write implementation code for login, register & sign-out in FirebaseAuthSource.kt class

--- app/src/main/java/com/farhan/twitter/data/remote/FirebaseAuthSource.kt
+++ app/src/main/java/com/farhan/twitter/data/remote/FirebaseAuthSource.kt
@@ -1,25 +1,48 @@
 package com.farhan.twitter.data.remote
 
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import io.reactivex.rxjava3.core.Completable
+import javax.inject.Inject
 
 /**
  * Created by Mohd Farhan on 5/5/2021.
  */
-class FirebaseAuthSource : IAuthSource{
+
+class FirebaseAuthSource @Inject constructor(
+     private val firebaseAuth: FirebaseAuth,
+     private val firebaseFirestore: FirebaseFirestore) : IAuthSource{
 
     override fun register(email: String, password: String, name: String): Completable {
-        TODO("Not yet implemented")
+        return Completable.create { emitter ->
+            firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnFailureListener { e -> emitter.onError(e) }
+                .addOnCompleteListener { //create new user
+                    val map = HashMap<String, Any>()
+                    map["email"] = email
+                    map["name"] = name
+                    map["imageUrl"] = "default"
+                    firebaseFirestore.collection("users")
+                        .document(userId()).set(map)
+                        .addOnFailureListener { e -> emitter.onError(e) }
+                        .addOnSuccessListener { emitter.onComplete() }
+                }
+        }
     }
 
     override fun login(email: String, password: String): Completable {
-        TODO("Not yet implemented")
+        return Completable.create { emitter ->
+            firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnFailureListener { e -> emitter.onError(e) }
+                .addOnSuccessListener { emitter.onComplete() }
+        }
     }
 
     override fun signOut(){
-        TODO("Not yet implemented")
+        firebaseAuth.signOut()
     }
 
     override fun userId(): String {
-        TODO("Not yet implemented")
+        return firebaseAuth.currentUser?.uid ?:""
     }
 }
