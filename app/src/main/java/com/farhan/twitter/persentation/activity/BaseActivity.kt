4d9f326fdc7f4a69e63f2ea627c1acdd839b8540
mofarhan <60867101+mofarhan@users.noreply.github.com>
2021-05-06 12:51:08 +0400

    1. Add Jetpack compose dependency 2. Create BaseActivity class

---
+++ app/src/main/java/com/farhan/twitter/persentation/activity/BaseActivity.kt
@@ -0,0 +1,10 @@
+package com.farhan.twitter.persentation.activity
+
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ *   Created by Mohd Farhan on 06/05/2021.
+ */
+abstract class BaseActivity : AppCompatActivity() {
+
+}
